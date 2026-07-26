6755a388f65265a8106345f8cb3b71b855cddccb
thai <thaithaimimi@gmail.com>
2026-07-27 01:19:27 +0700

    target mt7981: add support ZX7981PM variant

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2803,13 +2803,31 @@ define Device/wirelesstag_zx7981pge
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 51200k
+  IMAGE_SIZE := 114688k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += wirelesstag_zx7981pge
+
+define Device/wirelesstag_zx7981pm
+  DEVICE_VENDOR := Wireless-Tag
+  DEVICE_MODEL := ZX7981PM
+  DEVICE_DTS := mt7981b-wirelesstag-zx7981pm
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware kmod-usb3 mt7981-wo-firmware
+  DEVICE_DTS_LOADADDR := 0x44000000
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += wirelesstag_zx7981pm
 
 define Device/wirelesstag_zx7981pd
   DEVICE_VENDOR := Wireless-Tag
@@ -2821,7 +2839,7 @@ define Device/wirelesstag_zx7981pd
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 51200k
+  IMAGE_SIZE := 114688k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
